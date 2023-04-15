0a661307731c27dcaecdaa3ad8ec698326a7b9a9
yasmine <graneyasmine@gmail.com>
2023-04-15 16:57:23 +0200

    djikstra_fonctionnel

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -17,7 +17,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() { 
-        boolean fini=false;
+       
+       boolean fini=false;
         final ShortestPathData data = getInputData();
         Graph graph = data.getGraph();
         ShortestPathSolution solution = null;
@@ -28,7 +29,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         // Initialize array of distances.
         // Double[] distances = new Double[nbNodes];
 
-        ArrayList<Label> label_array= new ArrayList<Label>();
+       // ArrayList<Label> label_array= new ArrayList<Label>();
+        /*  
         for (int i=0; i<nbNodes; i++){
             if (graph.getNodes().get(i)==data.getOrigin()){ 
                 label_array.set(i,new Label(graph.getNodes().get(i), 0.0));
@@ -86,6 +88,65 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         Collections.reverse(arc_array);
             
         return new ShortestPathSolution( data,  Status.FEASIBLE , new Path(graph, arc_array) ) ;
-    }
+    }*/
+    //les variables
+    boolean fini=false;
+    final ShortestPathData data = getInputData();
+    Graph graph = data.getGraph();
+    ShortestPathSolution solution = null; 
+    final int nbNodes = graph.size();
+    Label label_success = null; 
+    BinaryHeap<Label> labels_heap = new BinaryHeap<Label>();
 
+    //tableau des labels
+    Label [] Labels =new Label[nbNodes]; 
+    Labels[data.getOrigin().getId()].setCout_realise(0);
+
+    //ok insertion faite dans le tas
+    labels_heap.insert(Labels[data.getOrigin().getId()]);
+    // l algo se temine une fois arrivé à la destination ou tas vide 
+    while (!labels_heap.isEmpty() && !fini ){
+        //on retire le min du tas et on le marue;
+        Label label_noeud_actuel =labels_heap.deleteMin();
+        label_noeud_actuel.setMarque(true);
+        //
+        if (label_noeud_actuel.getSommet_courant() == data.getDestination()){
+           fini=true;
+        }
+
+        for (Arc les_succeseurs : label_noeud_actuel.getSommet_courant().getSuccessors() ){
+            Label label_les_succeseurs=Labels[les_succeseurs.getDestination().getId()];
+            if (!label_les_succeseurs.getmarque()){
+                //si le cout du succes est sup au cout du predecess+cout de l arc
+                if (label_les_succeseurs.getCout_realise()>label_noeud_actuel.getCout_realise()+data.getCout_realise(les_succeseurs)){
+                    if (Labels[label_les_succeseurs.getSommet_courant().getId().getCout_realise()!=POSITIVE_INFINITY]){
+                        labels_heap.remove(label_les_succeseurs);
+                    }
+                    //on met à jour le cout
+                    label_les_succeseurs.setCout_realise(label_noeud_actuel.getCout_realise()+data.getCout_realise(les_succeseurs));
+                    labels_heap.insert(label_les_succeseurs);
+                    label_les_succeseurs.setPere(les_succeseurs)
+                }
+            }
+
+ }
 }
+
+ if (labels[ data.getDestination().getId() ]==null || !fini ||){
+    solution=new ShortestPathSolution(data, Status.INFEASIBLE);
+}
+else{
+
+    // The destination has been found, notify the observers.
+    notifyDestinationReached(data.getDestination());
+    ArrayList<Arc> liste_arcs = new ArrayList< >();
+    Arc arc_array=Labels[data.getDestination().getId()].getPere();
+    while (arc_array!=null){
+        liste_arcs.add(arc_array);
+        arc_array= Labels[arc_array.getOrigin().getId()].getPere();
+    }
+    Collections.reverse(arc_array);
+    solution=new new ShortestPathSolution(data, Status.INFEASIBLE);//to change
+}
+return solution;
+ }
